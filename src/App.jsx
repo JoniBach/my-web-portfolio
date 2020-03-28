e74d4004ef0893c9928776746cf72661991e2f61
JoniBach <james@jamescrook.uk>
2020-03-28 18:43:12 +0000

    Adding material ui component lib

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import PersonPinIcon from '@material-ui/icons/PersonPin';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box display="flex" justifyContent="center" alignItems="center">
+      <PersonPinIcon />
+      <Typography>James&apos; portfolio comming soon! </Typography>
+    </Box>
   );
 }
 
