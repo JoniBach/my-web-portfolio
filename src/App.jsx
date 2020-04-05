23b71825eba92b0260ca96de5324346dc4d7a3de
student <student@James-MacBook-Pro.local>
2020-04-05 01:23:33 +0100

    Adding switches ready for new page

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@
 /* eslint-disable no-tabs */
 import React from 'react';
 import MainDisplay from './components/MainDisplay/MainDisplay';
+import DemoSite from './components/DemoSite/DemoSite';
+import Button from '@material-ui/core/Button'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
 function App() {
     return (
-        <div>
-            <MainDisplay />
-        </div>
+        <Router>   
+                <Switch>
+                    <Route exact path='/' component={MainDisplay} />
+                    <Route exact path='/demosite' component={DemoSite} />
+                </Switch>
+        </Router>
     );
 }
 
--- src/components/MainDisplay/MainDisplay.jsx
+++ src/components/MainDisplay/MainDisplay.jsx
@@ -10,21 +10,24 @@ import DriveIcon from '@material-ui/icons/InsertDriveFile';
 import logo from './portrait-01.jpg';
 import useStyles from './MainDisplay.style';
 import LinkToButtonsComponent from '../LinkToButtonsComponent/LinkToButtonsComponent';
+import MenuBookIcon from '@material-ui/icons/MenuBook';
 
 
 const buttons = [
     {buttonText: 'LinkedIn', buttonIcon: <LinkedInIcon />, buttonlink:'https://www.linkedin.com/in/james-crook-492652185/'},
     {buttonText: 'Email', buttonIcon: <EmailIcon />, buttonlink: 'mailto:james@jamescrook.uk'},
-    {buttonText: 'My CV', buttonIcon: <DriveIcon />, buttonlink: 'https://docs.google.com/document/d/1T7bBXvHs8SR6-YXDmXWAdowlkUAd7ONjOdUKwZ8-tvk/edit?usp=sharing?export=download' },
-    {buttonText: 'Instagram', buttonIcon: <Instagram />, buttonlink: 'https://www.instagram.com/jzcrook/'}
+    // {buttonText: 'Portfolio', buttonIcon: <MenuBookIcon />, buttonlink: '/demosite'},
+    {buttonText: 'CV', buttonIcon: <DriveIcon />, buttonlink: 'https://docs.google.com/document/d/1T7bBXvHs8SR6-YXDmXWAdowlkUAd7ONjOdUKwZ8-tvk/edit?usp=sharing?export=download' },
+    {buttonText: 'Instagram', buttonIcon: <Instagram />, buttonlink: 'https://www.instagram.com/jzcrook/'},
+
 ]
 
 export default function MainDisplay() {
     const styles = useStyles();
 
     return (
-        <Box align="center" justifyContent="center" alignContent="center" display="flex" className={styles.fullScreen}>
-            <Box alignSelf="center" className={styles.root} >
+        <Box width="100%" align="center" justifyContent="center" alignContent="center" display="flex" className={styles.fullScreen}>
+            <Box width="100%" alignSelf="center" className={styles.root} >
                 <img src={logo} alt="My logo" className={styles.image} />
                 <Typography color="primary" variant="h4">
                         James Crook
@@ -32,7 +35,9 @@ export default function MainDisplay() {
                 <Typography color="primary" variant="h6">
                         Graduate Developer
                 </Typography>
+                <Box py={1}>
                 <LinkToButtonsComponent buttons={buttons} />
+                </Box>
             </Box>
         </Box>
     );
