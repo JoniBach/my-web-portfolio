94cb6a89edb501a241d86ff1267b07b880465120
JoniBach <james@jamescrook.uk>
2020-04-13 18:46:15 +0100

    Adding structure for my portfolio

--- src/components/DemoSite/DemoSite.jsx
+++ src/components/DemoSite/DemoSite.jsx
@@ -2,16 +2,12 @@
 /* eslint-disable react/jsx-indent */
 /* eslint-disable no-tabs */
 import React from 'react';
-import Toolbar from '@material-ui/core/Toolbar';
-import makeStyles from './DemoSite.style'
-import { Typography, Box } from '@material-ui/core';
-import LinkToButtons from '../LinkToButtonsComponent/LinkToButtonsComponent'
 import logo from './DemoSiteImages/wing_logo_01.png'
 import NavBar from '../NavBar/NavBar'
 
 const NavContent = [
     { buttonText: 'Home', buttonlink: '/' },
-    { buttonText: 'My Projects', buttonlink: '' },
+    { buttonText: 'My Projects', buttonlink: '/demosite' },
 ]
 
 const NavImages = [
