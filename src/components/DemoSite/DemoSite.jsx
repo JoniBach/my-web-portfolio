e27af8d5471a78a40702af49eb43e0d75f0e2fc0
student <student@James-MacBook-Pro.local>
2020-04-13 23:41:49 +0100

    Making cards render dynamically from array of objects

--- src/components/DemoSite/DemoSite.jsx
+++ src/components/DemoSite/DemoSite.jsx
@@ -9,20 +9,48 @@ import Box from '@material-ui/core/Box'
 
 
 const NavContent = [
+    /* 
+    Each Object can take the following peramiters:  
+    buttonText: String
+    buttonlink: String
+    buttonIcon: Array<Object>
+    buttonColor: String
+    buttonlink: String
+    */
     { buttonText: 'Home', buttonlink: '/' },
     { buttonText: 'My Projects', buttonlink: '/demosite' },
 ]
 
 const NavImages = [
+    /* 
+    Each Object can take the following peramiters:  
+    navImage: String
+    */
     { navImage: logo },
+]
+
+const CardData = [
+    /* 
+    Each Object can take the following peramiters:  
+    image: String
+    title: String
+    description: String
+    href: String
+    */
+    { image: 'https://www.wallpaperbetter.com/wallpaper/429/1000/856/basket-of-puppies-1080P-wallpaper-middle-size.jpg', title: 'Hi', description: 'hello', href: '/'},
+    { image: 'https://www.wallpaperbetter.com/wallpaper/429/1000/856/basket-of-puppies-1080P-wallpaper-middle-size.jpg'},
+    { title: 'Hi', description: 'hello', href: '/'},
+    
+
+
 ]
 
 export default function DemoSite() {
     return (
-        <Box >
+        <Box>
             <NavBar navContent={NavContent} navImages={NavImages} />
             <Box p={3}>
-            <ProjectCard />
+                <ProjectCard cardData={CardData} />
             </Box>
         </Box>
 
