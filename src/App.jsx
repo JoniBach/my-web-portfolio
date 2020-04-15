6b4cd3a5481fde300a0991afa0d1ed663f715159
student <student@James-MacBook-Pro.local>
2020-04-15 01:17:04 +0100

    Disabling router until i can figure out a fix

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,9 @@ function App() {
     return (
         <Router>   
                 <Switch>
-<<<<<<< HEAD
-                    <Route exact path='/' component={MainDisplay} />
-                    <Route exact path='#/demosite' component={DemoSite} />
-=======
                     <Route exact path='/' component={DemoSite} />
                     <Route path='/home' component={MainDisplay} />
-
                     {/* <Route exact path='/demosite' component={DemoSite} /> */}
->>>>>>> Disabling router until i can figure out a fix
                 </Switch>
         </Router>
     );
