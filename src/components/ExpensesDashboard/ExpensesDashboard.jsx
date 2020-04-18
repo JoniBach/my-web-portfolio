0655804bd7aa3006633e0d4b3fa8dfda36a02445
student <student@James-MacBook-Pro.local>
2020-04-19 00:36:39 +0100

    Adding content for the expenses dashboard including place holders for chart.js content

--- src/components/ExpensesDashboard/ExpensesDashboard.jsx
+++ src/components/ExpensesDashboard/ExpensesDashboard.jsx
@@ -1,78 +1,3 @@
-// /* eslint-disable linebreak-style */
-// /* eslint-disable react/jsx-indent */
-// /* eslint-disable no-tabs */
-// import React from 'react';
-// import { Typography, Checkbox, FormGroup, FormControlLabel } from '@material-ui/core';
-// import NavBar from '../NavBar/NavBar'
-// import ProjectCard from '../ProjectCard/ProjectCard'
-// import Box from '@material-ui/core/Box'
-// import logo from '../DemoSite/DemoSiteImages/wing_logo_01.png'
-
-// const NavContent = [
-//     /*
-//     Each Object can take the following peramiters:  
-//     buttonText: String
-//     buttonlink: String
-//     buttonIcon: Array<Object>
-//     buttonColor: String
-//     buttonlink: String
-//     */
-//     { buttonText: 'Home', buttonlink: '/home' },
-//     { buttonText: 'Expenses Dash', buttonlink: '/expensesdashboard' },
-//     { buttonText: 'My Work', buttonlink: '/' },
-
-// ]
-
-// const NavImages = [
-//     /* 
-//     Each Object can take the following peramiters:  
-//     navImage: String
-//     */
-//     { navImage: logo },
-// ]
-// export default function DemoSite() {
-//     const [showWebDesign, setShowWebDesign] = React.useState(true);
-//     const handleShowWebDesign = (event) => {
-//         setShowWebDesign(event.target.checked);
-//     };
-
-
-//     return (
-//         <Box>
-//             <NavBar navContent={NavContent} navImages={NavImages} />
-//             <Box px={3}>
-//                 <FormGroup row>
-//                 <FormControlLabel
-//                   control={(
-//                             <Checkbox
-//                                 checked={showWebDesign}
-//                                 onChange={handleShowWebDesign}
-//                                 inputProps={{ 'aria-label': 'primary checkbox' }}
-//                             />
-//                         )}
-//                         label="Web Design"
-//                     />
-//                 </FormGroup>
-//             </Box>
-
-//             <Box px={3}>
-//                 <FormGroup row>
-//                 {showWebDesign ? (
-//                         <div>
-//                             <Box pt={4} pb={1}>
-//                                 <Typography variant="h4" component="h2">Web Design</Typography>
-//                             </Box>
-//                         </div>
-//                     )
-//                         : null}
-//                 </FormGroup>
-
-//             </Box>
-//         </Box>
-
-//     );
-// }
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
@@ -81,18 +6,24 @@ import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
 import Hidden from '@material-ui/core/Hidden';
 import IconButton from '@material-ui/core/IconButton';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import Checkbox from '@material-ui/core/Checkbox';
 import ListItemText from '@material-ui/core/ListItemText';
-import MailIcon from '@material-ui/icons/Mail';
 import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
-import { Grid, Paper, Box } from '@material-ui/core';
+import { Grid, Paper, Box, Button } from '@material-ui/core';
+import DoughnutPlaceholder from './ExpensesDashboardImages/ChartjsDoughnutPlaceholder.png';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import AddIcon from '@material-ui/icons/AddCircleOutlineOutlined';
 
 const drawerWidth = 240;
 
@@ -139,40 +70,63 @@ function ResponsiveDrawer(props) {
         setMobileOpen(!mobileOpen);
     };
 
+    const [showWhat, setShowWhat] = React.useState(true);
+    const handleShowWhat = (event) => {
+        setShowWhat(event.target.checked);
+    };
+    const [showWhere, setShowWhere] = React.useState(true);
+    const handleShowWhere = (event) => {
+        setShowWhere(event.target.checked);
+    };
+    const [showHowMuch, setShowHowMuch] = React.useState(true);
+    const handleShowHowMuch = (event) => {
+        setShowHowMuch(event.target.checked);
+    };
+
+    function createData(name, totalHighestSpend, storeName, dateHightestSpend, addReciept) {
+        return { name, totalHighestSpend, storeName, dateHightestSpend, addReciept };
+    }
+
+    const rows = [
+        createData(1, '£430.00', 'Goldsmiths', '16/02/20', ),
+        createData(2, '£134.79', 'Ebay', '12/02/20', ),
+        createData(3, '£67.99', 'Argos', '18/02/20', ),
+        createData(4, '£55.00', 'John Lewis', '22/02/20', ),
+        createData(5, '£49.99', 'Amazon', '02/02/20', ),
+    ];
+
     const drawer = (
         <div>
             <div className={classes.toolbar} />
             <Divider />
             <List>
-                <ListItem button >
+                <ListItem>
                     <ListItemIcon>
                         <Checkbox
-                            checked={'showWebDesign'}
-                            onChange={'handleShowWebDesign'}
+                            checked={showWhat}
+                            onChange={handleShowWhat}
                             inputProps={{ 'aria-label': 'primary checkbox' }}
                         />
                     </ListItemIcon>
                     <ListItemText primary="What have you bought?" />
                 </ListItem>
-            </List>
-            <List>
-                <ListItem button >
+                <ListItem >
                     <ListItemIcon>
                         <Checkbox
-                            checked={'showWebDesign'}
-                            onChange={'handleShowWebDesign'}
+                            checked={showWhere}
+                            onChange={handleShowWhere}
                             inputProps={{ 'aria-label': 'primary checkbox' }}
                         />
                     </ListItemIcon>
                     <ListItemText primary="Where have you shopped?" />
                 </ListItem>
-            </List>
-            <List>
-                <ListItem button >
+
+
+                <ListItem >
                     <ListItemIcon>
                         <Checkbox
-                            checked={'showWebDesign'}
-                            onChange={'handleShowWebDesign'}
+                            checked={showHowMuch}
+                            onChange={handleShowHowMuch}
                             inputProps={{ 'aria-label': 'primary checkbox' }}
                         />
                     </ListItemIcon>
@@ -235,22 +189,81 @@ function ResponsiveDrawer(props) {
             </nav>
             <main className={classes.content}>
                 <div className={classes.toolbar} />
+
                 <Grid container>
-                    <Grid item xs={6}>
-                        <Box p={2}>
-                            <Paper >hi</Paper>
-                        </Box>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <Box p={2}>
-                            <Paper>hi</Paper>
-                        </Box>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Box p={2}>
-                            <Paper>hi</Paper>
-                        </Box>
-                    </Grid>
+                    {showWhat ? (
+                        <Grid item xs={6}>
+                            <Box p={2} width="100%" align="center" justifyContent="center" alignContent="center">
+                                <Paper >
+                                    <Box width="100%" alignSelf="center" py={1}>
+                                        <Typography >
+                                            Purchase Type
+                                        </Typography>
+                                        <img src={DoughnutPlaceholder} width={200}></img>
+
+                                    </Box>
+                                </Paper>
+                            </Box>
+                        </Grid>
+                    )
+                        : null}
+
+                    {showWhere ? (
+                        <Grid item xs={6}>
+                            <Box p={2} width="100%" align="center" justifyContent="center" alignContent="center">
+                                <Paper >
+                                    <Box width="100%" alignSelf="center" py={1}>
+                                        <Typography >
+                                            Purchase Details
+                                        </Typography>
+                                        <img src={DoughnutPlaceholder} width={200}></img>
+                                    </Box>
+                                </Paper>
+                            </Box>
+                        </Grid>
+                    )
+                        : null}
+                    {showHowMuch ? (
+                        <Grid item xs={12}>
+                            <Box p={2} width="100%" align="center" justifyContent="center" alignContent="center">
+                                <Paper >
+                                    <Box width="100%" alignSelf="center" p={2}>
+                                        <Typography >
+                                            Top Purchase
+                                    </Typography>
+                                        <TableContainer component={Paper}>
+                                            <Table className={classes.table} aria-label="simple table">
+                                                <TableHead>
+                                                    <TableRow>
+                                                        <TableCell>Rank</TableCell>
+                                                        <TableCell>Total</TableCell>
+                                                        <TableCell>Store</TableCell>
+                                                        <TableCell>Date</TableCell>
+                                                        <TableCell>Add Reciepit</TableCell>
+                                                    </TableRow>
+                                                </TableHead>
+                                                <TableBody>
+                                                    {rows.map((row) => (
+                                                        <TableRow key={row.name}>
+                                                            <TableCell component="th" scope="row">
+                                                                {row.name}
+                                                            </TableCell>
+                                                            <TableCell>{row.totalHighestSpend}</TableCell>
+                                                            <TableCell>{row.storeName}</TableCell>
+                                                            <TableCell>{row.dateHightestSpend}</TableCell>
+                                                            <TableCell><Button><AddIcon /></Button></TableCell>
+                                                        </TableRow>
+                                                    ))}
+                                                </TableBody>
+                                            </Table>
+                                        </TableContainer>
+                                        
+                                    </Box>
+                                </Paper>
+                            </Box>
+                        </Grid>
+                    )
+                        : null}
                 </Grid>
             </main>
         </div>
