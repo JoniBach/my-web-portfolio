9cf585e81c62ad306f7a2636769aaf62eb97a20b
student <student@James-MacBook-Pro.local>
2020-04-22 01:27:01 +0100

    removing hard coded data

--- src/components/ExpensesDashboard/ExpensesDashboard.jsx
+++ src/components/ExpensesDashboard/ExpensesDashboard.jsx
@@ -40,26 +40,6 @@ import { ResponsiveCalendar } from '@nivo/calendar'
 
 
 export default function ExpensesDashboard(props) {
-    
-    const data = [{
-        id: "css",
-        label: "css",
-        value: 313,
-        color: "hsl(174, 70%, 50%)",
-    },
-    {
-        id: "make",
-        label: "make",
-        value: 375,
-        color: "hsl(207, 70%, 50%)",
-    },
-    {
-        id: "dog",
-        label: "dog",
-        value: 327,
-        color: "hsl(115, 70%, 50%)",
-    },
-    ];
 
     const [newStoreName, setStoreName] = useState('');
 
