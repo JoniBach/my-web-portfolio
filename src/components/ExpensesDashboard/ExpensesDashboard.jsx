c1b8da38187209445bc320542d9b1dca8898dd24
student <student@James-MacBook-Pro.local>
2020-04-24 14:42:08 +0100

    Editing ring graph to be mobile apropriate

--- src/components/ExpensesDashboard/ExpensesDashboard.jsx
+++ src/components/ExpensesDashboard/ExpensesDashboard.jsx
@@ -377,13 +377,12 @@ export default function ExpensesDashboard(props) {
                         </Grid>
                     )
                         : null}
-                    
                     {/* rendering purchase type card if checkbox is true */}
                     {showPurchaseType ? (
                         <Grid item xs={6}>
                             <Box p={2} width="100%" align="center" justifyContent="center" alignContent="center">
                                 <Paper>
-                                    <Box width="100%" alignSelf="center" py={1}>
+                                    <Box width="100%" alignSelf="center" py={1} overflow="auto">
                                         <Typography>
                                             Purchase Type
                                         </Typography>
@@ -391,7 +390,7 @@ export default function ExpensesDashboard(props) {
                                             <ResponsivePie
                                                 data={newPurchase}
                                                 margin={{
-                                                    top: 40, right: 80, bottom: 80, left: 80,
+                                                    top: 20, right: 20, bottom: 20, left: 20,
                                                 }}
                                                 innerRadius={0.5}
                                                 padAngle={0.7}
@@ -399,6 +398,7 @@ export default function ExpensesDashboard(props) {
                                                 colors={{ scheme: 'nivo' }}
                                                 borderWidth={1}
                                                 borderColor={{ from: 'color', modifiers: [['darker', 0.2]] }}
+                                                enableRadialLabels={false}
                                                 radialLabelsSkipAngle={10}
                                                 radialLabelsTextXOffset={6}
                                                 radialLabelsTextColor="#333333"
@@ -409,6 +409,7 @@ export default function ExpensesDashboard(props) {
                                                 radialLabelsLinkColor={{ from: 'color' }}
                                                 slicesLabelsSkipAngle={10}
                                                 slicesLabelsTextColor="#333333"
+                                                sliceLabel="id"
                                                 animate
                                                 motionStiffness={90}
                                                 motionDamping={15}
@@ -422,10 +423,10 @@ export default function ExpensesDashboard(props) {
                         : null}
                     {/* rendering purchase type card if checkbox is true */}
                     {showRecentStores ? (
-                        <Grid item xs={6}>
+                        <Grid item xs={6} >
                             <Box p={2} width="100%" align="center" justifyContent="center" alignContent="center">
                                 <Paper>
-                                    <Box width="100%" alignSelf="center" py={1}>
+                                    <Box width="100%" alignSelf="center" py={1} overflow="auto">
                                         <Typography>
                                             Recent Stores
                                         </Typography>
