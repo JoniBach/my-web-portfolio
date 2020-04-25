1b3917eb336fdafe57703d3196bacdda139999f6
student <student@James-MacBook-Pro.local>
2020-04-25 17:33:04 +0100

    adding sorting for existing components and removing redundant recent stores

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -10,6 +10,7 @@ import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 import Typography from '@material-ui/core/Typography';
 import { Grid } from '@material-ui/core';
+import { ResponsiveCalendar } from '@nivo/calendar'
 
 
 
@@ -39,6 +40,33 @@ export default function MediaCard({ cardData }: cardDataProps) {
 
 
 const matches = useMediaQuery(theme.breakpoints.up('sm'));
+
+const MyResponsiveCalendar = ({ data /* see data tab */ }) => (
+    <ResponsiveCalendar
+        data={data}
+        from="2015-03-01"
+        to="2016-07-12"
+        emptyColor="#eeeeee"
+        colors={[ '#61cdbb', '#97e3d5', '#e8c1a0', '#f47560' ]}
+        margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
+        yearSpacing={40}
+        monthBorderColor="#ffffff"
+        dayBorderWidth={2}
+        dayBorderColor="#ffffff"
+        legends={[
+            {
+                anchor: 'bottom-right',
+                direction: 'row',
+                translateY: 36,
+                itemCount: 4,
+                itemWidth: 42,
+                itemHeight: 36,
+                itemsSpacing: 14,
+                itemDirection: 'right-to-left'
+            }
+        ]}
+    />
+)
 
 
     return (
