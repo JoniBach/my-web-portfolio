2d4c82cc51d53d73a5599e71bef53f15e7fccee4
student <student@James-MacBook-Pro.local>
2020-05-10 15:02:31 +0100

    adding proxt for cors

--- src/components/Croamer/Croamer.jsx
+++ src/components/Croamer/Croamer.jsx
@@ -13,7 +13,7 @@ class Croamer extends React.Component {
 
     };
     async componentDidMount() {
-        const covidData = 'https://opendata.ecdc.europa.eu/covid19/casedistribution/json';
+        const covidData = '/covid19/casedistribution/json';
         const res = await fetch(covidData);
         const data = await res.json();
         this.setState({ record: data.records, loading: false });
