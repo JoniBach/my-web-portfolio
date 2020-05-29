e96a4f5459f71eb6f61708be9a9553184146d28c
student <student@James-MacBook-Pro.local>
2020-05-29 18:46:19 +0100

    Adding user entry for new data

--- src/components/Croamer/Croamer.jsx
+++ src/components/Croamer/Croamer.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-indent-props */
 /* eslint-disable react/jsx-indent */
 import React, { useState, useEffect } from 'react';
-import { Slider, Paper, LinearProgress, Typography, Box, Grid } from '@material-ui/core';
+import { Slider, Paper, LinearProgress, Typography, Box, Grid, Button } from '@material-ui/core';
 import {
     FlexibleWidthXYPlot,
     XAxis,
@@ -15,13 +15,17 @@ import {
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import '../../../node_modules/react-vis/dist/style.css';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 
 export default function ButtonAppBar() {
+    const [selectedLocation, setSelectedLocation] = useState();
     const [loading, setLoading] = useState(true);
     const [casesRange, setCasesRange] = React.useState(60000);
     const handleChangeCasesRange = (event, newValue) => {
         setCasesRange(newValue);
     };
+    const [locationData, setLocationData] = useState([]);
     const [selectedDate, setSelectedDate] = React.useState(new Date());
     const [startDate, setStartDate] = React.useState(new Date('02/01/2020'));
     const unixStartDate = startDate.getTime();
@@ -38,6 +42,8 @@ export default function ButtonAppBar() {
             const response = await fetch(url);
             const json = await response.json();
             const xy = json.records.map(e => ({ x: new Date(`${e.month}/${e.day}/${e.year}`), y: e.cases, size: e.deaths, location: e.countriesAndTerritories }));
+            const locations = json.records.map(e => ({ location: e.countriesAndTerritories }));
+            setLocationData(locations);
             setData(xy);
             setLoading(false);
         }
@@ -47,10 +53,26 @@ export default function ButtonAppBar() {
 
         return [data, loading];
     };
-
     const [covidData] = useFetch(
         "https://cors-anywhere.herokuapp.com/https://opendata.ecdc.europa.eu/covid19/casedistribution/json"
     )
+    const uniq = new Set(locationData.map(e => JSON.stringify(e)));
+    const uniqueLocations = Array.from(uniq).map(e => JSON.parse(e));
+
+  
+    const [newCountry, setNewCountry] = useState([]);
+
+    const addCountryToGraph = (event) => {
+        event.preventDefault();
+        setNewCountry([
+            ...newCountry,
+            {selectedLocation}
+        ]);
+    };
+
+    console.log(newCountry);
+
+
     return (
         <div>
             <div>
@@ -129,10 +151,15 @@ export default function ButtonAppBar() {
                                                         xPercent={-0.03}
                                                         yPercent={0.06}
                                                     />
-                                                    <LineSeries color="green" data={covidData.filter(d => d.location === 'Italy')} />
+                                                    <LineSeries color="green" data={covidData.filter(d => d.location === selectedLocation)} />
                                                     <LineSeries color="blue" data={covidData.filter(d => d.location === 'United_Kingdom')} />
                                                     <LineSeries color="red" data={covidData.filter(d => d.location === 'China')} />
                                                     <LineSeries color="orange" data={covidData.filter(d => d.location === 'United_States_of_America')} />
+                                                    {
+                                                        newCountry.map(e => (
+                                                        <LineSeries color="green" data={covidData.filter(d => d.location === e.selectedLocation.location)} />
+                                                        ))
+                                                    }
                                                 </FlexibleWidthXYPlot>
                                             </Box>
                                             <Box pl={11} pr={5} pb={3} display="flex">
@@ -146,6 +173,30 @@ export default function ButtonAppBar() {
                                                     valueLabelDisplay="off"
                                                 />
                                             </Box>
+                                            <Box display="flex">
+                                                <Autocomplete
+                                                    id="combo-box-demo"
+                                                    value={selectedLocation}
+                                                    // onChange={(e) => setSelectedLocation(e.target.value)}
+                                                    onChange={(event, newValue) => {
+                                                        setSelectedLocation(newValue);
+                                                    }}
+                                                    options={uniqueLocations}
+                                                    getOptionLabel={(option) => option.location}
+                                                    fullWidth={true}
+                                                    renderInput={(params) => <TextField {...params} fullWidth={true} label="Select Month" variant="outlined" />}
+
+                                                />
+                                                <Button
+                                                    variant="outlined"
+                                                    color="primary"
+                                                    onClick={addCountryToGraph}
+                                                >
+                                                    add
+                                                </Button>
+
+                                            </Box>
+                                            
                                         </Paper>
                                     </Grid>
                                     <Grid item xs={12} paper>
@@ -155,7 +206,14 @@ export default function ButtonAppBar() {
                                                     <span style={{ color: 'blue' }}> United Kingdom </span> |
                                                     <span style={{ color: 'orange' }}> United States  </span> |
                                                     <span style={{ color: 'green' }}> Italy </span> |
-                                                    <span style={{ color: 'red' }}> China </span> |
+                                                    <span style={{ color: 'red' }}> China </span>
+                                                    <br />
+                                                    {
+                                                        newCountry.map(e => (
+                                                        <span>{e.selectedLocation.location} | </span>
+                                                        ))
+                                                    }
+                                                <span>{}</span>
                                                 </Typography>
                                             </Box>
                                         </Paper>
@@ -165,5 +223,108 @@ export default function ButtonAppBar() {
                         </div>
                     )}
         </div>
-    )
+    );
 }
+
+const top100Films = [
+    { title: 'The Shawshank Redemption', year: 1994 },
+    { title: 'The Godfather', year: 1972 },
+    { title: 'The Godfather: Part II', year: 1974 },
+    { title: 'The Dark Knight', year: 2008 },
+    { title: '12 Angry Men', year: 1957 },
+    { title: "Schindler's List", year: 1993 },
+    { title: 'Pulp Fiction', year: 1994 },
+    { title: 'The Lord of the Rings: The Return of the King', year: 2003 },
+    { title: 'The Good, the Bad and the Ugly', year: 1966 },
+    { title: 'Fight Club', year: 1999 },
+    { title: 'The Lord of the Rings: The Fellowship of the Ring', year: 2001 },
+    { title: 'Star Wars: Episode V - The Empire Strikes Back', year: 1980 },
+    { title: 'Forrest Gump', year: 1994 },
+    { title: 'Inception', year: 2010 },
+    { title: 'The Lord of the Rings: The Two Towers', year: 2002 },
+    { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
+    { title: 'Goodfellas', year: 1990 },
+    { title: 'The Matrix', year: 1999 },
+    { title: 'Seven Samurai', year: 1954 },
+    { title: 'Star Wars: Episode IV - A New Hope', year: 1977 },
+    { title: 'City of God', year: 2002 },
+    { title: 'Se7en', year: 1995 },
+    { title: 'The Silence of the Lambs', year: 1991 },
+    { title: "It's a Wonderful Life", year: 1946 },
+    { title: 'Life Is Beautiful', year: 1997 },
+    { title: 'The Usual Suspects', year: 1995 },
+    { title: 'Léon: The Professional', year: 1994 },
+    { title: 'Spirited Away', year: 2001 },
+    { title: 'Saving Private Ryan', year: 1998 },
+    { title: 'Once Upon a Time in the West', year: 1968 },
+    { title: 'American History X', year: 1998 },
+    { title: 'Interstellar', year: 2014 },
+    { title: 'Casablanca', year: 1942 },
+    { title: 'City Lights', year: 1931 },
+    { title: 'Psycho', year: 1960 },
+    { title: 'The Green Mile', year: 1999 },
+    { title: 'The Intouchables', year: 2011 },
+    { title: 'Modern Times', year: 1936 },
+    { title: 'Raiders of the Lost Ark', year: 1981 },
+    { title: 'Rear Window', year: 1954 },
+    { title: 'The Pianist', year: 2002 },
+    { title: 'The Departed', year: 2006 },
+    { title: 'Terminator 2: Judgment Day', year: 1991 },
+    { title: 'Back to the Future', year: 1985 },
+    { title: 'Whiplash', year: 2014 },
+    { title: 'Gladiator', year: 2000 },
+    { title: 'Memento', year: 2000 },
+    { title: 'The Prestige', year: 2006 },
+    { title: 'The Lion King', year: 1994 },
+    { title: 'Apocalypse Now', year: 1979 },
+    { title: 'Alien', year: 1979 },
+    { title: 'Sunset Boulevard', year: 1950 },
+    { title: 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb', year: 1964 },
+    { title: 'The Great Dictator', year: 1940 },
+    { title: 'Cinema Paradiso', year: 1988 },
+    { title: 'The Lives of Others', year: 2006 },
+    { title: 'Grave of the Fireflies', year: 1988 },
+    { title: 'Paths of Glory', year: 1957 },
+    { title: 'Django Unchained', year: 2012 },
+    { title: 'The Shining', year: 1980 },
+    { title: 'WALL·E', year: 2008 },
+    { title: 'American Beauty', year: 1999 },
+    { title: 'The Dark Knight Rises', year: 2012 },
+    { title: 'Princess Mononoke', year: 1997 },
+    { title: 'Aliens', year: 1986 },
+    { title: 'Oldboy', year: 2003 },
+    { title: 'Once Upon a Time in America', year: 1984 },
+    { title: 'Witness for the Prosecution', year: 1957 },
+    { title: 'Das Boot', year: 1981 },
+    { title: 'Citizen Kane', year: 1941 },
+    { title: 'North by Northwest', year: 1959 },
+    { title: 'Vertigo', year: 1958 },
+    { title: 'Star Wars: Episode VI - Return of the Jedi', year: 1983 },
+    { title: 'Reservoir Dogs', year: 1992 },
+    { title: 'Braveheart', year: 1995 },
+    { title: 'M', year: 1931 },
+    { title: 'Requiem for a Dream', year: 2000 },
+    { title: 'Amélie', year: 2001 },
+    { title: 'A Clockwork Orange', year: 1971 },
+    { title: 'Like Stars on Earth', year: 2007 },
+    { title: 'Taxi Driver', year: 1976 },
+    { title: 'Lawrence of Arabia', year: 1962 },
+    { title: 'Double Indemnity', year: 1944 },
+    { title: 'Eternal Sunshine of the Spotless Mind', year: 2004 },
+    { title: 'Amadeus', year: 1984 },
+    { title: 'To Kill a Mockingbird', year: 1962 },
+    { title: 'Toy Story 3', year: 2010 },
+    { title: 'Logan', year: 2017 },
+    { title: 'Full Metal Jacket', year: 1987 },
+    { title: 'Dangal', year: 2016 },
+    { title: 'The Sting', year: 1973 },
+    { title: '2001: A Space Odyssey', year: 1968 },
+    { title: "Singin' in the Rain", year: 1952 },
+    { title: 'Toy Story', year: 1995 },
+    { title: 'Bicycle Thieves', year: 1948 },
+    { title: 'The Kid', year: 1921 },
+    { title: 'Inglourious Basterds', year: 2009 },
+    { title: 'Snatch', year: 2000 },
+    { title: '3 Idiots', year: 2009 },
+    { title: 'Monty Python and the Holy Grail', year: 1975 },
+];
