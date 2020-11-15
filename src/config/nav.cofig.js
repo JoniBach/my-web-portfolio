9d8532403ec0326aaf01efd428aad240c316246b
student <student@James-MacBook-Pro.local>
2020-11-15 16:05:18 +0000

    Added previous tech test websites to nav

--- src/config/nav.cofig.js
+++ src/config/nav.cofig.js
@@ -7,6 +7,9 @@ import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import InsertDriveFileIcon from '@material-ui/icons/InsertDriveFile';
 import GitHubIcon from '@material-ui/icons/GitHub';
+import LocalBarIcon from '@material-ui/icons/LocalBar';
+import MovieIcon from '@material-ui/icons/Movie';
+import PetsIcon from '@material-ui/icons/Pets';
 export const pages = [
     { label: 'About', link: '/', icon: <InfoIcon /> },
     { label: 'Creative', link: '/creativeportfolio', icon: <CreateIcon /> },
@@ -18,4 +21,7 @@ export const links = [
     { label: 'Instagram', link: 'https://www.instagram.com/infrageist/', icon: <InstagramIcon /> },
     // { label: 'CV', link: 'https://github.com/JoniBach', icon: <InsertDriveFileIcon /> },
     { label: 'GITHub', link: 'https://github.com/JoniBach', icon: <GitHubIcon /> },
+    { label: 'Buy a Brew', link: 'https://jonibach.github.io/buy-a-brew/', icon: <LocalBarIcon /> },
+    { label: 'Stellify Media', link: 'https://jonibach.github.io/stellify-media/', icon: <MovieIcon /> },
+    { label: 'My Kitten Bestie', link: 'https://jonibach.github.io/gsap-tut-01/', icon: <PetsIcon /> },
 ]
