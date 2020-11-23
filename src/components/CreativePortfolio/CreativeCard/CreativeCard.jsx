c03b24432622ba5765538455c6efe513b0bbef6e
James Crook <jamescrook@CIC001080.local>
2020-11-23 00:05:43 +0000

    Adding project view modal functionality

--- src/components/CreativePortfolio/CreativeCard/CreativeCard.jsx
+++ src/components/CreativePortfolio/CreativeCard/CreativeCard.jsx
@@ -5,8 +5,14 @@ import React, { useState } from "react";
 import "./CreativeDashboard.css";
 
 export const MediaCard = (props) => {
+  console.log(props);
   return (
-  <a href={props.link} className="card" onclick="window.open('newurl.html','mywindow');" style={{ backgroundImage: `url(${props.image})` }} />
+    <div
+      // href={props.link}
+      className="card"
+      // onclick="window.open('newurl.html','mywindow');"
+      style={{ backgroundImage: `url(${props.image})` }}
+    />
   );
 };
 
@@ -18,18 +24,56 @@ export default function CreativeCard(props) {
   const top = `${props.position[1]}%`;
   const left = `${props.position[0]}%`;
   const [flip, setFlip] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  // TODO: map this over and write it to a new modal
+  console.log(props);
 
   if (flip) {
     return (
-      <div
-        style={{position: "relative", width: "100%",}}
-        >
+      <>
         <div
-style={{ backgroundImage: `url(${props.image})` }}           >
-          {props.children}
+          style={{ position: "relative", width: "100%" }}
+          onClick={() => setShowModal(true)}
+        >
+          <div style={{ backgroundImage: `url(${props.image})` }}>
+            {props.children}
+          </div>
+          <button className="icon-button" onClick={() => setFlip(false)}>
+            X
+          </button>
         </div>
-        <button className="icon-button" onClick={() => setFlip(false)}>X</button>
-      </div>
+        {showModal ? (
+          <div className="modal-container">
+            <button
+              className="icon-button"
+              onClick={() => setShowModal(false)}
+              style={{
+                position: "fixed",
+                top: "5%",
+              }}
+            >
+              X
+            </button>
+            {props.children.props.children.map((d, i) =>
+              d.props.type === "iframe" ? (
+                <iframe
+                  className="modal-content"
+                  height="100%"
+                  width="100%"
+                  src={`https://www.youtube.com/embed/${d.props.embedCode}`}
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen
+                ></iframe>
+              ) : (
+                <img src={d.props.image} className="modal-content" />
+              )
+            )}
+          </div>
+        ) : (
+          <div />
+        )}
+      </>
     );
   } else {
     return (
--- src/components/CreativePortfolio/CreativeDashboard.jsx
+++ src/components/CreativePortfolio/CreativeDashboard.jsx
@@ -1,39 +1,40 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable react/jsx-indent */
 /* eslint-disable no-tabs */
-import React, {useState} from "react";
-import CreativeCard, { MediaCard, CardCollection } from "./CreativeCard/CreativeCard";
-import {Data} from "../../config/creative.config"
+import React, { useState } from "react";
+import CreativeCard, {
+  MediaCard,
+  CardCollection,
+} from "./CreativeCard/CreativeCard";
+import { Data } from "../../config/creative.config";
 import NavBar from "../NavBar/NavBar";
 
 export default function CreativeDashboard() {
-
   return (
-    <div style={{backgroundColor: 'black'}}>
-        <NavBar style="fab" />
-    {
-        Data.map((d,i) => (
-            <CreativeCard
-            image={d.content[0].image}
-            text={d.title}
-            button={true}
-            position={d.textPosition}
-            color={d.textColor}
-            width={150}
-            textAlign={d.textAlign}
-          >
-          <CardCollection >
-              {
-              d.content.map((c,i) => (
-                <MediaCard link={c.href} image={c.image}/>
-              ))
-              }
-      
+    <div style={{ backgroundColor: "black" }}>
+      <NavBar style="fab" />
+      {Data.map((d, i) => (
+        <CreativeCard
+          image={d.content[0].image}
+          text={d.title}
+          button={true}
+          position={d.textPosition}
+          color={d.textColor}
+          width={150}
+          textAlign={d.textAlign}
+        >
+          <CardCollection>
+            {d.content.map((c, i) => (
+              <MediaCard
+                type={c.type}
+                embedCode={c.embedCode ? c.embedCode : undefined}
+                link={c.href}
+                image={c.image}
+              />
+            ))}
           </CardCollection>
-          </CreativeCard>
-        ))
-    }
-
+        </CreativeCard>
+      ))}
     </div>
   );
 }
