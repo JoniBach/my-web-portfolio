55c2f04b030668a683f3cc8d1a9a9e2149541962
James Crook <jamescrook@CIC001080.local>
2020-11-29 18:59:19 +0000

    Adding new web portfolio page

---
+++ src/config/web.config.js
@@ -0,0 +1,36 @@
+/* 
+    Each Object can take the following peramiters:  
+    image: String
+    title: String
+    description: String
+    href: String
+    */
+export const Data = [
+  {
+      image: "https://jamescrookdev.s3.eu-west-2.amazonaws.com/images/WebScreenShots/BuyABrew.png",
+    title: "Buy a Brew",
+    href: "https://jonibach.github.io/buy-a-brew/",
+  textPosition: [50, 30],
+  textColor: "white",
+  textAlign: "center",
+
+},
+  {
+    image: "https://jamescrookdev.s3.eu-west-2.amazonaws.com/images/WebScreenShots/Stellify.png",
+    title: "Stellify Media",
+    href: "https://jonibach.github.io/stellify-media/",
+  textPosition: [50, 30],
+  textColor: "white",
+  textAlign: "center",
+
+},
+  {
+    image: "https://jamescrookdev.s3.eu-west-2.amazonaws.com/images/WebScreenShots/Cats.png",
+    title: "My Kitten Bestie",
+    href: "https://jonibach.github.io/gsap-tut-01/",
+  textPosition: [50, 30],
+  textColor: "black",
+  textAlign: "center",
+
+},
+];
