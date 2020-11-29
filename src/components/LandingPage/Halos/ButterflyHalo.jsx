9adae098144e6ecc741860154c88375038fea54f
James Crook <jamescrook@CIC001080.local>
2020-11-29 01:47:30 +0000

    Imroving landing page style

--- src/components/LandingPage/Halos/ButterflyHalo.jsx
+++ src/components/LandingPage/Halos/ButterflyHalo.jsx
@@ -10,14 +10,17 @@ import PixelOverlay from "../../../PixelOverlayS.png";
 import Gif1 from './AnimatedWebHaloV1.gif'
 
 export default function ButterflyHalo() {
-  const title1 = "James Crook";
+  const title1 = "Artsy Tech Stuff";
   const title2 = "Creative Practitioner";
   return (
     <div >
+
       {/* <video autoPlay loop muted>
         <source src={Clip} type="video/mp4" />
       </video> */}
-      <img className="video" src={Gif1} />
+      {/* <img className="video" src={Gif1} /> */}
+      {/* <div className="video" style={{backgroundImage: `url(${Gif1})`}} /> */}
+
       <div className="content">
         <svg viewBox="0 0 220 120">
           <defs>
@@ -78,7 +81,7 @@ export default function ButterflyHalo() {
           </text>
           <text filter="url(#f1)" fill="magenta">
             <tspan
-              font-size="30"
+              font-size="28"
               x="50.2%"
               y="40.2%"
               dominant-baseline="middle"
@@ -89,7 +92,7 @@ export default function ButterflyHalo() {
           </text>
           <text filter="url(#f1)" fill="blue">
             <tspan
-              font-size="30"
+              font-size="28"
               x="49.8%"
               y="39.8%"
               dominant-baseline="middle"
@@ -100,7 +103,7 @@ export default function ButterflyHalo() {
           </text>
           <text fill="url(#rainbow)">
             <tspan
-              font-size="30"
+              font-size="28"
               x="50%"
               y="40%"
               dominant-baseline="middle"
@@ -143,9 +146,23 @@ export default function ButterflyHalo() {
             </tspan>
           </text>
         </svg>
-        <img src={PixelOverlay} className="pixelate" />
+        {/* <div className="pixelate" style={{
+                    backgroundImage: 'url('+PixelOverlay+')',
+                    backgroundSize: "cover",
+                    height: "100vh",
+          
+        }}></div> */}
+        {/* <img src={PixelOverlay} className="pixelate" /> */}
+        {/* <div className="pixelate" style={{backgroundImage: `url(${PixelOverlay})`}} /> */}
       </div>
       <NavBar style="fab" />
+      <div id="tv" className="media" style={{backgroundImage: `url(${Gif1})`,  zIndex: -1, }}/>
+      <div  className="tv-frame"/>
+    <div className="backdrop" />
+
+  
+      <div id="tv" style={{backgroundImage: `url(${PixelOverlay})`,  opacity: 0.1, zIndex: 1}}/>
+      {/* <div id="tv" style={{backgroundColor: 'red'}}/> */}
 
     </div>
   );
