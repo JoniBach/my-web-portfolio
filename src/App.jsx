b64670fcefa0cdf33311b1180d36a99ea6e2151e
James Crook <jamescrook@CIC001080.local>
2020-11-30 23:20:35 +0000

    Externalising expenses dash

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,8 @@ import React from 'react';
 import WebDash from './components/CreativePortfolio/WebDashboard';
 import CreativeDash from './components/CreativePortfolio/CreativeDashboard';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import ExpensesDashboard from './components/ExpensesDashboard/ExpensesDashboard';
 import DateFnsUtils from '@date-io/date-fns';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
-import LandingPage from './components/LandingPage/LandingPage';
 import Croamer from './components/Croamer/Croamer';
 import ButterflyHalo from './components/LandingPage/Halos/ButterflyHalo'
 import './App.css';
@@ -19,9 +17,7 @@ function App() {
             <MuiPickersUtilsProvider utils={DateFnsUtils}>
                 <Route exact path="/webportfolio" component={WebDash} />
                 <Route exact path="/creativeportfolio" component={CreativeDash} />
-                {/* <Route exact path="/" component={LandingPage} /> */}
                 <Route exact path="/" component={ButterflyHalo} />
-                {/* <Route exact path="/webportfolio" component={ExpensesDashboard} /> */}
                 <Route exact path="/mycovidtracker" component={Croamer} />
                 </MuiPickersUtilsProvider>
             </Switch>
